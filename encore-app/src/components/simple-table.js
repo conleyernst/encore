6bc7e45cbb449edbd67b5de2a50e30b85cb486e4
conleyernst <cernst3@u.rochester.edu>
2019-04-16 13:05:15 -0400

    table added

--- encore-app/src/components/simple-table.js
+++ encore-app/src/components/simple-table.js
@@ -7,6 +7,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Button from "@material-ui/core/es/Button/Button";
 
 const styles = {
     root: {
@@ -19,44 +20,98 @@ const styles = {
 };
 
 let id = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(title, artist, score) {
     id += 1;
-    return { id, name, calories, fat, carbs, protein };
+    return { id, title, artist, score };
 }
 
-const data = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+// const data = [
+//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//     createData('Eclair', 262, 16.0, 24, 6.0),
+//     createData('Cupcake', 305, 3.7, 67, 4.3),
+//     createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+const mockData = [
+    {
+        title: 'American Idiot',
+        artist: 'Greenday',
+        score: 5
+    },
+    {
+        title: 'American Idiot',
+        artist: 'Greenday',
+        score: 5
+    },
+    {
+        title: 'American Idiot',
+        artist: 'Greenday',
+        score: 5
+    },
+    {
+        title: 'American Idiot',
+        artist: 'Greenday',
+        score: 5
+    },
+    {
+        title: 'American Idiot',
+        artist: 'Greenday',
+        score: 5
+    },
+    {
+        title: 'American Idiot',
+        artist: 'Greenday',
+        score: 5
+    },
+]
 
 function SimpleTable(props) {
     const { classes } = props;
+
+
+    let data = [];
+    for (let i = 0; i < mockData.length; i++){
+        const element = mockData[i];
+        const obj = createData(element.title, element.artist, element.score);
+        data.push(obj);
+    }
+
+    //todo have this as a prop passed in
+    const isHost = true;
 
     return (
         <Paper className={classes.root}>
             <Table className={classes.table}>
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat (g)</TableCell>
-                        <TableCell align="right">Carbs (g)</TableCell>
-                        <TableCell align="right">Protein (g)</TableCell>
+                        <TableCell align="right">Title</TableCell>
+                        <TableCell align="right">Artist</TableCell>
+                        <TableCell align="right">Score</TableCell>
+                        <TableCell align="right">Vote</TableCell>
+                        {isHost &&
+                            <TableCell align="right">Veto</TableCell>
+                        }
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {data.map(n => (
                         <TableRow key={n.id}>
                             <TableCell component="th" scope="row">
-                                {n.name}
+                                {n.title}
                             </TableCell>
-                            <TableCell align="right">{n.calories}</TableCell>
-                            <TableCell align="right">{n.fat}</TableCell>
-                            <TableCell align="right">{n.carbs}</TableCell>
-                            <TableCell align="right">{n.protein}</TableCell>
+                            {/*<TableCell align="right">{n.title}</TableCell>*/}
+                            <TableCell align="right">{n.artist}</TableCell>
+                            <TableCell align="right">{n.score}</TableCell>
+                            <TableCell align="right">
+                                <Button variant="contained" color="primary">Yee</Button>
+                                <Button variant="contained" color="primary">Nee</Button>
+                            </TableCell>
+                            {isHost &&
+                                <TableCell align="right">
+                                    <Button variant="contained" color="primary">Veto</Button>
+                                </TableCell>
+                            }
                         </TableRow>
                     ))}
                 </TableBody>
