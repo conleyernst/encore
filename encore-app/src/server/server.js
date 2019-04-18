f575a4b736a5734286b669727875c6b1c0c8dd70
Dan Cancelmo <dcancelm@u.rochester.edu>
2019-04-18 14:10:02 -0400

    Server formatting, status codes, and crash on invalid update fixed

--- encore-app/src/server/server.js
+++ encore-app/src/server/server.js
@@ -6,7 +6,7 @@ const env = require('dotenv').config();
 const routes = express.Router();
 const PORT = 4000;
 
-let Song = require('./song.model');
+let Song = require('./song.model.js');
 
 // MIDDLEWARE
 app.use(bodyParser.json());
@@ -25,11 +25,9 @@ connection.once('open', function() {
 
 routes.route('/add').post(function (req, res) {
 	let song = new Song(req.body);
-	song.save()
-		.then(song => {
+	song.save().then(song => {
 			res.status(200).json({'song': 'song added successfully'});
-		})
-		.catch(err => {
+	}).catch(err => {
 		res.status(400).send('adding new song failed');
 	});
 });
@@ -38,8 +36,9 @@ routes.route('/').get(function (req, res) {
 	Song.find(function(err, songs) {
 		if (err) {
 			console.error(err);
+			res.status(400).send("Request for songs failed");
 		} else {
-			res.json(songs);
+			res.status(200).json(songs);
 		}
 	})
 });
@@ -47,50 +46,51 @@ routes.route('/').get(function (req, res) {
 routes.route('/update/:id').post(function(req, res) {
 	Song.findById(req.params.id, function(err, song) {
 		if (!song)
-			res.status(404).send("data is not found");
-		else
+			res.status(404).send("song is not found");
+		else {
 			song.votes = req.body.votes;
 
 			song.save().then(song => {
-				res.json('Song updated!');
+				res.status(200).json('Song updated!');
 			}).catch(err => {
 				res.status(400).send("Update not possible");
 			});
+		}
 	});
 });
 
 routes.route('/upvote/:id').post(function(req, res) {
 	Song.findById(req.params.id, function(err, song) {
 		if (!song)
-			res.status(404).send("data is not found");
-		else
+			res.status(404).send("song is not found");
+		else {
 			song.votes++;
-
-		song.save().then(song => {
-			res.json('Song updated!');
-		}).catch(err => {
-			res.status(400).send("Update not possible");
-		});
+			song.save().then(song => {
+				res.status(200).json('Song updated!');
+			}).catch(err => {
+				res.status(400).send("Update not possible");
+			});
+		}
 	});
 });
 
 routes.route('/downvote/:id').post(function(req, res) {
 	Song.findById(req.params.id, function(err, song) {
 		if (!song)
-			res.status(404).send("data is not found");
-		else
+			res.status(404).send("song is not found");
+		else {
 			song.votes--;
-
-		song.save().then(song => {
-			res.json('Song updated!');
-		}).catch(err => {
-			res.status(400).send("Update not possible");
-		});
+			song.save().then(song => {
+				res.status(200).json('Song updated!');
+			}).catch(err => {
+				res.status(400).send("Update not possible");
+			});
+		}
 	});
 });
 
 routes.route('/veto/:id').post(function(req, res) {
-	Song.find({_id: req.params.id}).remove().then(() => {
+	Song.find({_id: req.params.id}).deleteOne().then(() => {
 		res.status(200).json({'song': 'song removed successfully'});
 	}).catch(err => {
 		res.status(400).send('removing song failed');
@@ -106,4 +106,4 @@ app.use('/songs', routes);
 // Starting Server
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`)
-})
+});
